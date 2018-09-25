ecbe07a05a9b248377f90e082c3c19b1148d587d
tushar21 <tnikam21@gmail.com>
2018-09-25 22:55:53 +0530

    Updated elasticsearch api url

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,10 @@
 const CONFIG = {
     API_URL : 'http://localhost:3001/',
-    ELASTIC_URL : 'http://localhost:9200/',
+    ELASTIC_URL : 'http://104.248.64.80:9200/',
     //ELASTIC_URL : 'https://fa13bd95d6804eb39ea2224043d5f4db.us-east-1.aws.found.io:9243/kibana_sample_data_flights/',
     //ELASTIC_PASSWORD: '70Yeprsl1Trx3OCzZ33Yv4mL',
     ELASTIC_PASSWORD: 'changeme',
-    ELASTIC_USERNAME: 'elastic' 
-    
+    ELASTIC_USERNAME: 'elastic'
 }
 
 export default CONFIG;
